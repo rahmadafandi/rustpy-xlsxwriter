13194cba537187540e4fd7bac0f348c9c1a8f5c1
Rahmad Afandi <rahmadafandiii@gmail.com>
2025-01-01 16:17:00 +0700

    fix: version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import RustExtension
 
 setup(
     name="rustpy_xlsxwriter",
-    version="0.4.0",
+    version="0.0.1",
     description="A Rust-powered Excel library for Python",
     rust_extensions=[RustExtension("rustpy_xlsxwriter.rustpy_xlsxwriter", "Cargo.toml", binding="pyo3")],
     packages=["rustpy_xlsxwriter"],
